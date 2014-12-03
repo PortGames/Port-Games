1acee53076a622f81d3764b3b5411985fd42c9cc
Raideron <0901267@hr.nl>
2014-12-03 12:10:05 +0100

    Toevoeging Crane_v1
    Containers met sprites aangemaakt

---
+++ Crane_v1/ContainerRed.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ContainerRed here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ContainerRed extends Containers
+{
+    /**
+     * Act - do whatever the ContainerRed wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Crane_v1/ContainerYellow.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ContainerYellow here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ContainerYellow extends Containers
+{
+    /**
+     * Act - do whatever the ContainerYellow wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
